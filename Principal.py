2f99ab401752eeab559d4bc5d071daf1ba49ce59
Mjp2204 <159024773+Mjp2204@users.noreply.github.com>
2024-06-02 19:04:56 -0500

    Add files via upload
    Codigo principal con los ajustes

--- Principal.py
+++ Principal.py
@@ -1,43 +1,24 @@
 import pygame
 import sys
-#import Carros_enemigos
 from pygame.locals import *
-#import random
-#import json
 from datetime import datetime
-from tkinter import PhotoImage
-import os
-from PIL import Image, ImageTk
+from PIL import Image, ImageTk, ImageFilter
 import tkinter as tk
-from tkinter import simpledialog
 import customtkinter as CTk
-from carrito import Carrito
-
-pygame.init() 
- 
-
-ROJO = (255, 0, 0)
-VERDE_CLARO = (144, 238, 144)
-BLANCO = (255, 255, 255)
-NEGRO = (0, 0, 0)
-NARANJA = (255, 165, 0)
-AMARILLO = (255, 255, 0)
-AZUL = (0, 0, 139)
+from carrito import *
 
 
-ruta_abs = ("Imagenes\Menu_inicio.jpg") 
+# Ruta de las imágenes
+ruta_juego = 'C:/Users/lenovo/Dropbox/PP/FINAL/Imagenes/Fondo.jpg'
+ruta_fondo = 'C:/Users/lenovo/Dropbox/PP/FINAL/Imagenes/MenuInicio20.jpg'
+ruta_logo = 'C:/Users/lenovo/Dropbox/PP/FINAL/Imagenes/Logo.png'
 
- 
+
 root = tk.Tk()
 root.title('Highway Rush')
-root.geometry = ('40x60')
+root.geometry('800x920')
 
-###########################################################
-
-
-ruta_fondo = 'Imagenes\menu_inicio.jpeg' 
- 
-
+# Cargar la imagen de fondo
 try:
     image = Image.open(ruta_fondo)
     background_image = ImageTk.PhotoImage(image)
@@ -48,47 +29,90 @@ except Exception as e:
 # Crear un canvas para poner la imagen de fondo
 canvas = tk.Canvas(root, width=image.width, height=image.height)
 canvas.pack(fill="both", expand=True)
-
 canvas.create_image(0, 0, image=background_image, anchor="nw")
-
 canvas.image = background_image
 
+# Cargar imagen de fondo
 try:
-    image = Image.open(ruta_fondo)
-    background_image = ImageTk.PhotoImage(image)
+    fondo_jugar = Image.open(ruta_juego)
+    distorted_image = fondo_jugar.filter(ImageFilter.BLUR) 
+    distorted_image_tk = ImageTk.PhotoImage(distorted_image)
 except Exception as e:
     print(f"Error al cargar la imagen: {e}")
     exit(1)
 
+# Cargar el logo
+try:
+    image = Image.open(ruta_logo)
+    logo_image = ImageTk.PhotoImage(image)
+except Exception as e:
+    print(f"Error al cargar la imagen: {e}")
+    exit(1)
 
-root.call('wm', 'iconphoto', root._w, background_image)
+root.call('wm', 'iconphoto', root._w, logo_image)
 
 def jugar():
-    # Solicitar el nombre del usuario
-    nombre = tk.simpledialog.askstring("Nombre", "Por favor, ingrese su nombre:")
+    boton_jugar.place_forget()
+    boton_salir.place_forget()
+    # Mostrar los campos de entrada y botón en el canvas
+    nombre_label.place(x=490, y=500)
+    nombre_entry.place(x=490, y=540)
+    boton_iniciar.place(x=490, y=580)
 
-    # Verificar que el nombre no esté vacío
+
+def iniciar_juego():
+    nombre = nombre_entry.get()
+    nombre_label.place_forget()
+    nombre_entry.place_forget()
+    boton_iniciar.place_forget()
+    boton_salir.place_forget()
+
     if nombre:
-        iniciar_juego(nombre)
+        # Limpiar el canvas
+        canvas.delete("all")
+        try:
+            fondo_jugar = Image.open(ruta_juego)
+            distorted_image = fondo_jugar.filter(ImageFilter.BLUR) 
+            distorted_image_tk = ImageTk.PhotoImage(distorted_image)
+        except Exception as e:
+            print(f"Error al cargar la imagen: {e}")
+            return
 
-def iniciar_juego(nombre):
-    # Limpiar el canvas
-    canvas.delete("all")
+        canvas.create_image(0, 0, image=distorted_image_tk, anchor="nw")
+        canvas.image = distorted_image_tk  
+        mensaje = canvas.create_text(400, 460, text=f"Bienvenido, {nombre}! El juego comienza ahora.", fill="black", font=("8-bit Arcade In", 30))
 
-    canvas.create_text(400, 300, text=f"Bienvenido, {nombre}! El juego comienza ahora.", fill="black", font=("8-bit Arcade In", 24))
+        root.after(5000, lambda: canvas.delete(mensaje))
 
-nombre_label = tk.Label(root, text="Ingrese su nombre:", font=("8-bit Arcade In", 12))
-nombre_entry = tk.Entry(root, font=("8-bit Arcade In", 12))
-boton_iniciar = tk.Button(root, text="Iniciar juego", command=iniciar_juego, font=("8-bit Arcade In", 12), bd=2, fg="white", bg="green", width=14, height=2)
+    def cuenta_regresiva(contador):
+        if contador > 0:
+            canvas.delete("contador")
+            texto_contador = canvas.create_text(400, 460, text=str(contador), fill="black", font=("8-bit Arcade In", 100), tag="contador")
+            root.after(1000, lambda: cuenta_regresiva(contador - 1))
+        else:
+            canvas.delete("contador")
+            texto_start = canvas.create_text(400, 460, text="START!", fill="black", font=("8-bit Arcade In", 110))
 
+    root.after(5000, lambda: cuenta_regresiva(5))
+
+def salir():
+    root.quit()
+
+VERDE_CLARO = "#399183"
+nombre_label = tk.Label(root, text="Ingrese su nombre:", font=("8-bit Arcade In", 16))
+nombre_entry = tk.Entry(root, font=("8-bit Arcade In", 16))
+boton_iniciar = tk.Button(root, text="Iniciar juego", command=iniciar_juego, font=("8-bit Arcade In", 16), bd=3, fg="black", bg= VERDE_CLARO, width=14, height=2)
 
 VERDE_CLARO = "#399183"
 boton_jugar = tk.Button(text="JUGAR", command=jugar, font=("8-bit Arcade In", 16), bd=3, fg="black", bg=VERDE_CLARO, width=14, height=2)
-boton_jugar.place(x=520, y=450)
+boton_jugar.place(x=490, y=500)
+
+VERDE_CLARO = "#399183"
+boton_salir = tk.Button(text="SALIR", command=salir, font=("8-bit Arcade In", 16), bd=3, fg="black", bg=VERDE_CLARO, width=14, height=2)
+boton_salir.place(x=490, y=560)
+
 
 root.mainloop()
-
-
 
 Clock = pygame.time.Clock()
 
@@ -123,4 +147,4 @@ while True:
     pantalla.fill((0, 0, 0))  # Rellena la pantalla con negro
     carrito_usuario.dibujar(pantalla)
     pygame.display.flip()
-    Clock.tick(60)  # Limita el framerate a 60 FPS
+    Clock.tick(60) 
